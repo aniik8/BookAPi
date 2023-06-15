79efa4fc588fd738b760756e8d6b43d236aa4fc3
Aniket <aniketmessi3@gmail.com>
2023-06-15 22:18:30 +0530

    implemented all functionalities

--- BookStore/views.py
+++ BookStore/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from .models import Book
+from .serializers import BookSerializer
+class BookListAPIView(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
-# Create your views here.
+class BookCreateAPIView(CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookRetrieveAPIView(RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookUpdateAPIView(UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDestroyAPIView(DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
