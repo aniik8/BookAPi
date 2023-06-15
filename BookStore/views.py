79ac0a1b1de77e3cfabeb028a9c6e01b73159ad3
Aniket <aniketmessi3@gmail.com>
2023-06-15 22:36:03 +0530

    added comments

--- BookStore/views.py
+++ BookStore/views.py
@@ -2,26 +2,36 @@ from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView,
 from .models import Book
 from .serializers import BookSerializer
 
-#Implement views for the API endpoints.
+#Implementing views for the API endpoints.
+
+
 # View for listing all books
 class BookListAPIView(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
 
 # View for creating a new book
 class BookCreateAPIView(CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+
+
 # View for retrieving a specific book by its ID
 class BookRetrieveAPIView(RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+
+
 # View for updating the details of a book by its ID
 class BookUpdateAPIView(UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
 # View for deleting a book by its ID
 class BookDestroyAPIView(DestroyAPIView):
     queryset = Book.objects.all()
